6dabf7ff427e6ba361826b9598beeeb2830fc56d
Bart710 <trsv420@gmail.com>
2024-09-03 22:20:15 -0500

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { login } = require("../controllers/authController");
+const {
+  login,
+  createUser,
+  updateUser,
+  deleteUser,
+  getAllUsers,
+  changePassword,
+} = require("../controllers/authController");
+const {
+  authMiddleware,
+  adminMiddleware,
+} = require("../middleware/authMiddleware");
 
 router.post("/login", login);
+router.post("/users", authMiddleware, adminMiddleware, createUser);
+router.put("/users/:id", authMiddleware, adminMiddleware, updateUser);
+router.delete("/users/:id", authMiddleware, adminMiddleware, deleteUser);
+router.get("/users", authMiddleware, adminMiddleware, getAllUsers);
+router.post(
+  "/users/:id/change-password",
+  authMiddleware,
+  adminMiddleware,
+  changePassword
+);
 
 module.exports = router;
