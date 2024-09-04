ea419058081a5e23395436da2e88a14427eb38c8
Bart710 <trsv420@gmail.com>
2024-09-03 22:19:57 -0500

    Update messageRoutes.js

--- Backend/routes/messageRoutes.js
+++ Backend/routes/messageRoutes.js
@@ -4,10 +4,12 @@ const {
   createMessage,
   getAllMessages,
   updateMessage,
+  deleteMessage,
 } = require("../controllers/messageController");
 
 router.post("/messages", createMessage);
 router.get("/messages", getAllMessages);
 router.put("/messages/:id", updateMessage);
+router.delete("/messages/:id", deleteMessage);
 
 module.exports = router;
